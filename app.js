82a5f4fd0ad56705207f6b28e9df169816115fdf
DhruvPatel029 <104353994+DhruvPatel029@users.noreply.github.com>
2024-04-03 12:27:59 -0400

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 
 
-var port = process.env.PORT || 8000;
+var port = 8000;
 app.use(bodyParser.urlencoded({ 'extended': 'true' }));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
